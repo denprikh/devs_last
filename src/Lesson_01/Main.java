637b0c39ca81f72d8dea79e4379963c67b0f9c5e
denprikh <dprihodko@meta-devs.io>
2022-01-21 00:36:51 +0300

    Lesson_01

---
+++ src/Lesson_01/Main.java
@@ -0,0 +1,27 @@
+package Lesson_01;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int value1 = 0;
+        int value2 = 5;
+
+        int sum = value1 + value2;
+
+        System.out.println("Сумма " + sum);
+
+        int multiply = value1 * value2;
+        System.out.println("Произведение " + multiply);
+
+        int difference = value1 - value2;
+        System.out.println("Разность " + difference);
+
+        String userInput = new Scanner(System.in).next();
+        System.out.println(userInput);
+
+
+
+    }
+}
